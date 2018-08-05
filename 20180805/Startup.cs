43d0c7e93a717d2f44314bd2070bddf3f9317bfa
katrina.chuang <katrina.chuang@ileopard.com>
2018-08-05 18:17:37 +0800

    [Profile] Try fix SqliteException: SQLite Error 1: 'no such table: Servers'.

--- 20180805/Startup.cs
+++ 20180805/Startup.cs
@@ -40,6 +40,8 @@ namespace _20180805
 
             //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+			// Automatically perform database migration
+            services.BuildServiceProvider().GetService<AppSystemContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
