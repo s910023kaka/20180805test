a6cf1ec1a1d34a1354e4d1ca9c7bd801db39cb77
katrina.chuang <katrina.chuang@ileopard.com>
2018-08-05 18:32:33 +0800

    [Profile] Replace the default index page with app profile index page.

--- 20180805/Pages/Index.cshtml.cs
+++ 20180805/Pages/Index.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Application.Models;
 
-namespace _20180805.Pages
+namespace _20180805.Pages.AppProfiles
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly Application.Models.AppSystemContext _context;
 
+        public IndexModel(Application.Models.AppSystemContext context)
+        {
+            _context = context;
+        }
+
+        public IList<AppProfile> AppProfile { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            AppProfile = await _context.AppProfiles
+                .Include(a => a.Server).ToListAsync();
         }
     }
 }
