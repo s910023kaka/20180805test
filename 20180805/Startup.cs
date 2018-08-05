54f033a8a9e41a1afa346d9ec76fcd531f7b2201
katrina.chuang <katrina.chuang@ileopard.com>
2018-08-05 16:07:14 +0800

    [Profile] Test error HTTP Error 502.5 - Process Failure.

--- 20180805/Startup.cs
+++ 20180805/Startup.cs
@@ -26,9 +26,20 @@ namespace _20180805
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppSystemContext>(options =>
-                                                    options.UseSqlite(Configuration.GetConnectionString("AppSystemContext")));
-            services.AddMvc();
+            //services.AddDbContext<AppSystemContext>(options =>
+            //                                        options.UseSqlite(Configuration.GetConnectionString("AppSystemContext")));
+            //services.AddMvc();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
